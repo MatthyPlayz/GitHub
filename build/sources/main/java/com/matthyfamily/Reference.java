5d4a12c42d94d5c0b83dfca13284abf3adec6c41
MatthyPlayz <34276633+MatthyPlayz@users.noreply.github.com>
2019-03-20 17:01:45 -0400

    Initial commit

---
+++ build/sources/main/java/com/matthyfamily/Reference.java
@@ -0,0 +1,8 @@
+package com.matthyfamily;
+
+public class Reference {
+	public static final String MODID = "stm";
+	public static final String MODNAME = "Suppergerrie2's Tutorial Mod";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+}
